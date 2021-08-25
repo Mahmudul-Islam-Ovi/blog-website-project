7bcf19f2f48b0c4ead652818a413653ef7b18525
Asif Ahsan <asifahsan727@gmail.com>
2021-08-26 01:35:23 +0600

    new navbar + update errors

--- js/main.js
+++ js/main.js
@@ -2,42 +2,16 @@
 
 	"use strict";
 
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	var carousel = function() {
-		$('.featured-carousel').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:30,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:true,
-	    dots: true,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:2
-	      },
-	      1000:{
-	        items:3
-	      }
-	    }
-		});
-
-	};
-	carousel();
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
 
 })(jQuery);
